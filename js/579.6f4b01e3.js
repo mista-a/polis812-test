d0387fad270be0477c2cf7775c0c8472daf3ce27
mista-a <dvaanddva@gmail.com>
2022-08-22 23:47:30 +0300

    adding dist subtree

---
+++ js/579.6f4b01e3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpolis812_test"]=self["webpackChunkpolis812_test"]||[]).push([[579],{4579:function(t,s,r){r.r(s),r.d(s,{default:function(){return a}});var e=function(){var t=this,s=t._self._c;return s("div",{staticClass:"error"},[s("router-link",{staticClass:"link",attrs:{to:"/"}},[t._v("404 To home")])],1)},n=[],l={name:"ErrorView"},i=l,u=r(1001),o=(0,u.Z)(i,e,n,!1,null,"3c128b2d",null),a=o.exports}}]);
+//# sourceMappingURL=579.6f4b01e3.js.map
